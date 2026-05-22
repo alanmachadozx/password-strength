036012d74df59eeed5a3a583aa7da45d32f1bf0e
Alan Machado <alanpinheirorj22@gmail.com>
2026-05-22 17:39:34 -0300

    feat: add especialChar function

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-
+def verification(passoword : str):
+    if (len(passoword) < 8):
+        raise Exception('The password is too short')
+    
+    upperAndLowe(passoword)
+    hasANumber(passoword)
+    especialChar(passoword)
+    
 def upperAndLowe(text):
     upper = False
     lower = False
@@ -14,8 +21,16 @@ def upperAndLowe(text):
     if not lower:
         raise Exception('At least one lowercase letter is required.')
 
-def verification(passoword):
-    if (len(passoword) < 8):
-        raise Exception('The password is too short')
+def hasANumber(passw):
+    number = any(char.isdigit() for char in passw)
+    if not number:
+        raise Exception('At least one number is required.')
+
+def especialChar(passw):
     
-    upperAndLowe(passoword)
+    char = any(char.isalnum() for char in passw)
+    if not char:
+        raise Exception('At least one special character is required.')
+    if ' ' in passw: 
+        raise Exception('No spaces allowed')
+        
